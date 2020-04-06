b7e8fc3f2cf3b04d370fbfefd26d2053a124499e
Quentin Lassalle <qlassalle@gmail.com>
2020-04-06 19:11:19 +0200

    Specify non null columns in database.sql

--- database.sql
+++ database.sql
@@ -5,7 +5,7 @@ create table article
 	id int auto_increment
 		primary key,
 	name varchar(50) not null,
-	description text null,
+	description text not null,
 	rating tinyint null,
 	created_at timestamp default CURRENT_TIMESTAMP not null,
 	updated_at timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP
