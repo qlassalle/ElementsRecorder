746fc220c803d5d64b97231d8b2906449f099bc0
Quentin Lassalle <qlassalle@gmail.com>
2020-10-01 08:51:43 +0200

    [CHORE] Create a helpful script to start and then clean test database

---
+++ start_test_db.sh
@@ -0,0 +1,7 @@
+trap ctrl_c INT
+
+function ctrl_c() {
+  docker-compose --file src/test/resources/conf/docker-compose-test.yml down
+}
+
+docker-compose --file src/test/resources/conf/docker-compose-test.yml up --build --force-recreate
