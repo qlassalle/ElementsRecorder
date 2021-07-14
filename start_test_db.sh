a46f3e245791240b86fde45260dc7c67a3ecac3f
Quentin Lassalle <qlassalle@gmail.com>
2021-07-14 22:44:20 +0200

    [CHORE] Use test container instead of manually launching container and then tests

--- start_test_db.sh
+++
@@ -1,7 +0,0 @@
-trap ctrl_c INT
-
-function ctrl_c() {
-  docker-compose --file src/test/resources/conf/docker-compose-test.yml down
-}
-
-docker-compose --file src/test/resources/conf/docker-compose-test.yml up --build --force-recreate
